e6a925955f69fbeb00778233e62c49f63b1c5c30
Gyaase Elvis <kojogyaase@gmail.com>
2021-05-03 14:13:34 +0000

    Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import GlobeComponent from "./components/GlobeComponent";
+
+function App() {
+  return (
+    <div >
+   <GlobeComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GlobeComponent.js
@@ -0,0 +1,46 @@
+import Globe from 'react-globe.gl';
+import datasets from "./datasets/world_population.csv"
+
+import * as d3 from "d3"
+import React, {useEffect, useRef, useState} from 'react';
+
+function GlobeComponent() {
+    const globeEl = useRef();
+    const [popData, setPopData] = useState([]);
+
+    useEffect(() => {
+        // load data
+        fetch("./datasets/world_population.csv").then(res => res.text())
+            .then(csv => d3.csvParse(csv, ({ lat, lng, pop }) => ({ lat: +lat, lng: +lng, pop: +pop })))
+            .then(setPopData);
+    }, []);
+
+    useEffect(() => {
+        // Auto-rotate
+        globeEl.current.controls().autoRotate = true;
+        globeEl.current.controls().autoRotateSpeed = 0.1;
+    }, []);
+
+    const weightColor = d3.scaleSequentialSqrt(d3.interpolateYlOrRd)
+        .domain([0, 1e7]);
+
+    return <>
+        <Globe
+        ref={globeEl}
+        globeImageUrl={process.env.PUBLIC_URL+"/images/earth-dark.jpg"}
+        bumpImageUrl={process.env.PUBLIC_URL+"/images/earth-topology.png"}
+        backgroundImageUrl={process.env.PUBLIC_URL+"/images/night-sky.png"}
+
+        hexBinPointsData={popData}
+        hexBinPointWeight="pop"
+        hexAltitude={d => d.sumWeight * 6e-8}
+        hexBinResolution={4}
+        hexTopColor={d => weightColor(d.sumWeight)}
+        hexSideColor={d => weightColor(d.sumWeight)}
+        hexBinMerge={true}
+        enablePointerInteraction={false}
+    />
+    </>;
+}
+
+export default GlobeComponent;
